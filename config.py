9829c8463f54296101d429dbc3f277cd1590969b
FabVali <fabvali@petalmail.com>
2025-05-03 17:03:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from pydantic_settings import BaseSettings
 from typing import ClassVar
 
 class Settings(BaseSettings):
-    TOKEN: str = "MTM1NTk4MTY3MDY0NTUwMTk3Mg.GOVmV6.FO9dNZU_EUwW-IXe0W4Q_2dM-i2sSonrciR1vY"
+    TOKEN: str = "DISCORD_BOT_TOKEN"
     
 settings = Settings()
